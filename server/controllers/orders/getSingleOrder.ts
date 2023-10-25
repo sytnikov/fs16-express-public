e56e690e202234065a21ea9cb5eeeec1b3138b02
Md Shayemur Rahman <mdshayemur.rahman@tuni.fi>
2023-10-25 17:57:21 +0300

    fix on singleOrder method

--- server/controllers/orders/getSingleOrder.ts
+++ server/controllers/orders/getSingleOrder.ts
@@ -6,8 +6,12 @@ export const getSingleOrder = (req: Request, res: Response) => {
     const orderId = parseInt(req.params.id);
     const singleOrder = ordersData.find((order) => order.id === orderId);
 
-    singleOrder ? res.status(200).json(singleOrder) : 'No order exist!';
+    if (singleOrder) {
+      res.status(200).json(singleOrder);
+    } else {
+      res.status(404).json({ error: 'Order Not Found!' });
+    }
   } catch (error) {
-    res.status(404).json({ error: error });
+    res.status(500).json({ error: error });
   }
 };
