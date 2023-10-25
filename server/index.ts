90a1ebaf3a315521a5c11014bdcba91174b4af3c
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2023-10-25 18:24:57 +0300

    add user router to index

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,7 @@ import productsRouter from "./routes/productsRouter";
 import { loggingMiddleware } from "./middlewares/logging";
 import { apiErrorHandler } from "./middlewares/apiErrorHandler";
 import { categoriesRouter } from "./routes/categoriesRouter";
+import { usersRouter } from "./routes/usersRouter";
 
 const PORT = 8080;
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.json());
 
 app.use("/products", loggingMiddleware, productsRouter);
 app.use("/categories", loggingMiddleware, categoriesRouter);
+app.use("/users", loggingMiddleware, usersRouter);
 
 app.use(apiErrorHandler);
 app.listen(PORT, () => {
