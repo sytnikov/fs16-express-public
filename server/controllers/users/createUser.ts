72ce4454179bebfd94d5bb7e0c59043f493c1acc
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2023-10-25 22:15:06 +0300

    fix create user

--- server/controllers/users/createUser.ts
+++ server/controllers/users/createUser.ts
@@ -9,16 +9,57 @@ export function createUser(
     res: Response,
     next: NextFunction
     ) {
-    const newUser: User = req.body;
-    const nameExist = usersData.findIndex(
-        (cat) => cat.name.toLowerCase() === newUser.name.toLowerCase()
-    );
-    if (nameExist === -1) {
-            // need to add validation
-            newUser.id = usersData.length + 1;
-            usersData.push(newUser);
-            res.status(201).json(newUser);
-            return;
+    const { name, email, password, role } = req.body;
+    const newUser: User = {
+        id: usersData.length + 1,
+        name: name,
+        email: email,
+        password: password,
+        role: role,
+    };
+
+    if (!name || !email || !password || !role) {
+        next(ApiError.badRequest("Missing required fields"));
+        return;
     }
-    next(ApiError.badRequest("The category with this name already exists"));
+
+    if (password.length < 6) {
+        next(ApiError.badRequest("Password must be at least 6 characters"));
+        return;
+    }
+
+    if (!email.includes("@")) {
+        next(ApiError.badRequest("Invalid email"));
+        return;
+    }
+
+    if (role !== "admin" && role !== "user") {
+        next(ApiError.badRequest("Invalid role"));
+        return;
+    }
+
+    if (name.length < 2) {
+        next(ApiError.badRequest("Name must be at least 2 characters"));
+        return;
+    }
+
+    if (name.length > 20) {
+        next(ApiError.badRequest("Name must be less than 20 characters"));
+        return;
+    }
+
+    const emailExists = usersData.findIndex((user) => user.email === email);
+    const idExists = usersData.find((user) => user.id === newUser.id);
+
+    if (idExists) {
+        next(ApiError.badRequest("The user with this id already exists"));
+        return;
+    }
+    
+    if (emailExists === -1) {
+        usersData.push(newUser);
+        res.status(201).json(newUser);
+        return;
+    }
+    next(ApiError.badRequest("The user with this email already exists"));
 }
--- server/index.ts
+++ server/index.ts
@@ -9,6 +9,7 @@ import { usersRouter } from "./routes/usersRouter";
 const PORT = 8080;
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
