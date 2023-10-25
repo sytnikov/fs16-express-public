58d9d3ed7e2f5ef509939c5f7e05be610005c8c3
Md Shayemur Rahman <mdshayemur.rahman@tuni.fi>
2023-10-25 17:51:21 +0300

    Implement Order Router methods

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,18 @@
 import express from 'express';
 
-import productsRouter from "./routes/productsRouter";
-import { loggingMiddleware } from "./middlewares/logging";
-import { apiErrorHandler } from "./middlewares/apiErrorHandler";
-import { categoriesRouter } from "./routes/categoriesRouter";
+import productsRouter from './routes/productsRouter';
+import { loggingMiddleware } from './middlewares/logging';
+import { apiErrorHandler } from './middlewares/apiErrorHandler';
+import { categoriesRouter } from './routes/categoriesRouter';
+import ordersRouter from './routes/ordersRouter';
 
 const PORT = 8080;
 const app = express();
 app.use(express.json());
 
-app.use("/products", loggingMiddleware, productsRouter);
-app.use("/categories", loggingMiddleware, categoriesRouter);
+app.use('/products', loggingMiddleware, productsRouter);
+app.use('/categories', loggingMiddleware, categoriesRouter);
+app.use('/orders', loggingMiddleware, ordersRouter);
 
 app.use(apiErrorHandler);
 app.listen(PORT, () => {
