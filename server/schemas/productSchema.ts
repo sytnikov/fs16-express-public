545fff9ef2fc4845f394f24c84818d6d93a4795d
Yaroslav Shtanko <81259415+Yaroslav1918@users.noreply.github.com>
2023-10-26 21:02:30 +0300

    remove validationProduct midl

--- server/schemas/productSchema.ts
+++ server/schemas/productSchema.ts
@@ -1,32 +1,36 @@
 import { z } from "zod";
 
-export const productSchema = z.object({
-  body: z.object({
-    name: z.string({
-      required_error: "Name is required",
-    }),
-    description: z.string({
-      required_error: "Description is required",
-    }),
-    price: z.number({
-      required_error: "Price is required",
-    }),
-    categoryId: z.number({
-      required_error: "CategoryId is required",
-    }),
-    images: z.array(
-      z.string({
-        required_error: "Images are required",
-      })
-    ),
-  }),
-});
+export const productSchema = z
+  .object({
+    body: z.object({
+      name: z.string({
+        required_error: "Name is required",
+      }),
+      description: z.string({
+        required_error: "Description is required",
+      }),
+      price: z.number({
+        required_error: "Price is required",
+      }),
+      categoryId: z.number({
+        required_error: "CategoryId is required",
+      }),
+      images: z.array(
+        z.string({
+          required_error: "Images are required",
+        })
+      ),
+    }).strict(),
+  })
+
 
 export const uptadeProductSchema = z
   .object({
-    name: z.string().optional(),
-    price: z.number().optional(),
-    description: z.string().optional(),
-    images: z.array(z.string()).optional(),
+    body: z.object({
+      name: z.string().optional(),
+      price: z.number().optional(),
+      description: z.string().optional(),
+      images: z.array(z.string()).optional(),
+    }).strict(),
   })
-  .strict();
+
