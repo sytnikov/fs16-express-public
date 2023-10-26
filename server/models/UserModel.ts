f4982c4eee1fb599681113c92eef5ab790c5aee0
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2023-10-26 22:18:08 +0300

    add models file

---
+++ server/models/UserModel.ts
@@ -0,0 +1,52 @@
+import { User } from "../types/User";
+
+export class UserRepo{
+    users = [
+        {
+            id: 1,
+            name: "Admin",
+            email: "admin@mail.com",
+            password: "admin",
+            role: "admin"
+        },
+        {
+            id: 2,
+            name: "John Doe",
+            email: "john@mail.com",
+            password: "john",
+            role: "user"
+        },
+        {
+            id: 3,
+            name: "Jane Doe",
+            email: "free@maill.com",
+            password: "jane",
+            role: "user"
+        }
+    ];
+
+    getAll(){
+        return this.users;
+    }
+
+    getSingle(userId: number){
+        const user = this.users.find(user => user.id === userId)
+        return user;
+    }
+
+    createUser(newUser: User){
+        this.users.push(newUser)
+        return newUser
+    }
+
+    updateUser(index: number, updatedUser: User){
+        this.users.splice(index, 1, updatedUser)
+        return updatedUser
+    }
+
+    deleteUser(index: number){
+        this.users.splice(index, 1)
+        return this.users
+    }
+};
+
