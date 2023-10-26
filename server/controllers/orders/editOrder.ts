ac36d7928b3ab290cb3d0202bd37ec8c2e23616c
Md Shayemur Rahman <mdshayemur.rahman@tuni.fi>
2023-10-26 11:20:23 +0300

    Orders Controller added

--- server/controllers/orders/editOrder.ts
+++ server/controllers/orders/editOrder.ts
@@ -3,25 +3,27 @@ import { ordersData } from '../../data/ordersData';
 
 export const editOrder = (req: Request, res: Response) => {
   try {
+    const id = req.body.products[0].productId;
+    const quantity = req.body.products[0].quantity;
     const orderId = parseInt(req.params.id);
     const updatedOrderData = {
       id: orderId,
       products: [
         {
-          productId: req.body.products[0].productId,
-          quantity: req.body.products[0].quantity,
+          productId: id,
+          quantity: quantity,
         },
       ],
     };
     const foundOrder = ordersData.findIndex((order) => order.id === orderId);
-    if (foundOrder<0) {
+    if (foundOrder < 0) {
       res.status(404).json({ error: 'Order not found' });
     } else {
       ordersData[foundOrder] = {
         ...ordersData[foundOrder],
         ...updatedOrderData,
       };
-      res.status(200).json({ data: "success" });
+      res.status(200).json({ data: 'success' });
     }
   } catch (error) {
     res.status(404).json({ error: error });
