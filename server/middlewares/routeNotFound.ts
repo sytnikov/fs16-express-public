b4c6a5a1255dd9f2ee2465ec49f05d092e66d372
Yaroslav Shtanko <81259415+Yaroslav1918@users.noreply.github.com>
2023-10-28 20:27:20 +0300

    added logic to routeNotFound

--- server/middlewares/routeNotFound.ts
+++ server/middlewares/routeNotFound.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { monitorRequest } from "./logging";
 
 export function routeNotFound(req: Request, res: Response, next: NextFunction) {
+  const paths = /^(\/products|\/categories|\/orders)/;
   res.status(404).json({ msg: "Route not found" });
-  monitorRequest(req, res, next, true);
+  if (!paths.test(req.originalUrl)) monitorRequest(req, res, next, true);
 }
