2999fcc1594647fb7cf4c818899fae604a8027a8
Yaroslav Shtanko <81259415+Yaroslav1918@users.noreply.github.com>
2023-10-28 17:07:11 +0300

    bugs fixed

--- server/middlewares/routeNotFound.ts
+++ server/middlewares/routeNotFound.ts
@@ -1,5 +1,7 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import { monitorRequest } from "./logging";
 
-export function routeNotFound (_: Request, res: Response) {
-  res.status(404).json({msg: "Route not found"})
+export function routeNotFound(req: Request, res: Response, next: NextFunction) {
+  res.status(404).json({ msg: "Route not found" });
+  monitorRequest(req, res, next, true);
 }
